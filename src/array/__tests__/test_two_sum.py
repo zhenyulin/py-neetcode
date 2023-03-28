f00df60a7471331aaab9eedeae6c6d5eee724942
Zhenyu Lin <zhenyu.lyn@gmail.com>
2023-03-28 16:56:21 +0100

    feat(array): 1. two sum

---
+++ src/array/__tests__/test_two_sum.py
@@ -0,0 +1,8 @@
+from src.array.two_sum import twoSum
+
+
+def test_two_sum():
+    # the problem has been constrained to have only one valid answer
+    assert twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert twoSum([3, 2, 4], 6) == [1, 2]
+    assert twoSum([3, 3], 6) == [0, 1]
---
+++ src/array/two_sum.py
@@ -0,0 +1,20 @@
+#
+# problem: https://leetcode.com/problems/two-sum/
+#
+
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    """find the indices of two numbers in `nums` that add up to `target`
+
+    we can use a Hashmap to store `target - n` at each index
+    and check if we find the matching n through `nums`
+
+    time complexity: O(N), space complexity: O(N)
+    """
+
+    match = {}
+
+    for i, n in enumerate(nums):
+        if n in match:
+            return [match[n], i]
+        match[target - n] = i
