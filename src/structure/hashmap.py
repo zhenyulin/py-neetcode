a6bed44802876c981f6279fa9c7b511dfc74a458
Zhenyu Lin <zhenyu.lyn@gmail.com>
2023-05-19 16:46:15 +0100

    refactor(structure/hashmap): update amortised time complexity

--- src/structure/hashmap.py
+++ src/structure/hashmap.py
@@ -1,3 +1,14 @@
+#
+# https://wiki.python.org/moin/TimeComplexity
+#
+# Common Collision Policies:
+# [x] Separate bucketing
+# [ ] Open Addressing: store in the next available empty slot in the array
+# can be implemented using linear probing or quadratic probing or [double hash]
+# [ ] Robinhood Hashing: add to the index, reorganise the bucket for ideal index
+#
+
+
 from typing import Iterator
 
 Input = int | float | str | tuple
@@ -12,63 +23,67 @@ class HashMap:
         time complexity: O(1)
         """
         self.capacity = capacity
-        self.map = [[] for _ in range(self.capacity)]
+        self.buckets = [[] for _ in range(self.capacity)]
         self.length = 0
+
+    def _hash_key(self, key: Input) -> int:
+        """time complexity: O(1)"""
+        return hash(key) % self.capacity
 
     def __len__(self) -> int:
         """time complexity: O(1)"""
         return self.length
 
     def __contains__(self, key: Input) -> bool:
-        """time complexity: O(1)"""
-        index = self._get_index(key)
-        for k, _ in self.map[index]:
+        """
+        average time complexity: O(1), assuming collisions are uncommon
+        amortised time complexity: O(N), in case of collisions
+        """
+        index = self._hash_key(key)
+        for k, _ in self.buckets[index]:
             if k == key:
                 return True
         return False
 
     def __iter__(self) -> Iterator:
-        """time complexity: O(1)"""
-        for chain in self.map:
-            for key, _ in chain:
+        """time complexity: O(N)"""
+        for bucket in self.buckets:
+            for key, _ in bucket:
                 yield key
 
     def __getitem__(self, key: Input) -> any:
-        """time complexity: O(N)
-        average time complexity: O(1) if there isn't many hash value collisions
         """
-        index = self._get_index(key)
-        for k, v in self.map[index]:
+        average time complexity: O(1), assuming collisions are uncommon
+        amortised time complexity: O(N), in case of collisions
+        """
+        index = self._hash_key(key)
+        for k, v in self.buckets[index]:
             if k == key:
                 return v
         raise KeyError(f"{key} not found")
 
     def __setitem__(self, key: Input, value: any = None) -> None:
-        """time complexity: O(N)
-        average time complexity: O(1) if there isn't many hash value collisions
-
-        common collision policies:
-        - Separate Chaining [used here]
-        - Open Addressing: store in the next available empty slot in the array
-        can be implemented using linear probing or quadratic probing or double hash
-        - Robinhood Hashing: add to the index, reorganise the bucket for ideal index
         """
-        index = self._get_index(key)
-        for pair in self.map[index]:
-            if pair[0] == key:
-                pair[1] = value
+        average time complexity: O(1), assuming collisions are uncommon
+        amortised time complexity: O(N), in case of collisions
+        """
+        index = self._hash_key(key)
+        for entry in self.buckets[index]:
+            if entry[0] == key:
+                entry[1] = value
                 return
-        self.map[index].append([key, value])
+        self.buckets[index].append([key, value])
         self.length += 1
 
     def __delitem__(self, key: Input) -> None:
-        """time complexity: O(N)
-        average time complexity: O(1) if there isn't many hash value collisions
         """
-        index = self._get_index(key)
-        for i, (k, _) in enumerate(self.map[index]):
+        average time complexity: O(1), assuming collisions are uncommon
+        amortised time complexity: O(N), in case of collisions
+        """
+        index = self._hash_key(key)
+        for i, (k, _) in enumerate(self.buckets[index]):
             if k == key:
-                del self.map[index][i]
+                del self.buckets[index][i]
                 self.length -= 1
                 return
         raise KeyError(f"{key} not found")
@@ -78,30 +93,26 @@ class HashMap:
     put = __setitem__
     remove = __delitem__
 
-    def _get_index(self, key: Input) -> int:
-        """time complexity: O(1)"""
-        return hash(key) % self.capacity
-
     def keys(self) -> list[Input]:
         """time complexity: O(N)"""
         keys = []
-        for chain in self.map:
-            for k, _ in chain:
+        for bucket in self.buckets:
+            for k, _ in bucket:
                 keys.append(k)
         return keys
 
     def values(self) -> list[any]:
         """time complexity: O(N)"""
         values = []
-        for chain in self.map:
-            for _, v in chain:
+        for bucket in self.buckets:
+            for _, v in bucket:
                 values.append(v)
         return values
 
     def items(self) -> list[tuple[Input, any]]:
         """time complexity: O(N)"""
         items = []
-        for chain in self.map:
-            for pair in chain:
+        for bucket in self.buckets:
+            for pair in bucket:
                 items.append(tuple(pair))
         return items
