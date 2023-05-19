7daa907d412380de1f81db15e70bceecb02e3c84
Zhenyu Lin <zhenyu.lyn@gmail.com>
2023-05-19 17:52:48 +0100

    fix(structure/deque): add getter and setter

--- src/structure/__tests__/test_deque.py
+++ src/structure/__tests__/test_deque.py
@@ -3,53 +3,77 @@ from src.structure.deque import Deque
 
 
 class TestDeque:
-    def testLeft(self):
+    def testInitIter(self):
+        q = Deque([1, 2, 3])
+        assert list(q) == [1, 2, 3]
+
+    def testGetter(self):
+        q = Deque([1, 2, 3])
+        assert q[0] == 1
+        assert q[1] == 2
+        assert q[2] == 3
+        assert q[-1] == 3
+        assert q[-2] == 2
+        assert q[-3] == 1
+
+        with raises(IndexError):
+            q[-4]
+
+        with raises(IndexError):
+            q[3]
+
+    def testSetter(self):
+        q = Deque([1, 2, 3])
+        q[0] = 4
+        assert q[0] == 4
+        q[-1] = 0
+        assert q[-1] == 0
+
+        with raises(IndexError):
+            q[4] = 0
+
+        with raises(IndexError):
+            q[-4] = 0
+
+    def testIsEmpty(self):
+        q = Deque()
+        assert q.is_empty()
+        assert not q
+        q = Deque([1])
+        assert not q.is_empty()
+        assert q
+
+    def testAppendLeft(self):
         q = Deque()
         q.append_left(3)
         q.append_left(2)
         q.append_left(1)
-        assert q.right() == 3
-        assert q.left() == 1
+        assert list(q) == [1, 2, 3]
+        assert q[0] == 1
+
+    def testPopLeft(self):
+        q = Deque([1, 2, 3])
         assert q.pop_left() == 1
-        assert q.left() == 2
         assert q.pop_left() == 2
-        assert q.left() == 3
         assert q.pop_left() == 3
 
-    def testRight(self):
+    def testAppend(self):
         q = Deque()
+        q.append(1)
         q.append(2)
         q.append(3)
-        q.append(4)
-        assert q.right() == 4
-        assert q.left() == 2
-        q.append_left(1)
-        assert q.left() == 1
-        assert q.pop() == 4
+        assert list(q) == [1, 2, 3]
+        assert q[-1] == 3
+
+    def testPop(self):
+        q = Deque([1, 2, 3])
         assert q.pop() == 3
         assert q.pop() == 2
         assert q.pop() == 1
 
     def testRemove(self):
-        q = Deque()
-        q.append(2)
-        q.append(3)
-        q.append(4)
-        q.remove(3)
-        assert q.pop() == 4
-        assert q.pop() == 2
-
-    def testError(self):
-        q = Deque()
-
-        with raises(IndexError):
-            q.left()
-
-        with raises(IndexError):
-            q.right()
-
-        with raises(IndexError):
-            q.pop_left()
-
-        with raises(IndexError):
-            q.pop()
+        q = Deque([1, 2, 3])
+        q.remove(2)
+        assert list(q) == [1, 3]
+        q.remove(4)
+        assert list(q) == [1, 3]
--- src/structure/deque.py
+++ src/structure/deque.py
@@ -1,3 +1,12 @@
+#
+# https://wiki.python.org/moin/TimeComplexity
+#
+# using a index_map {} for O(1) getter and setter
+# would result in appendLeft and PopLeft being O(N)
+#
+from typing import Optional
+
+
 class Node:
     def __init__(self, val: any = None) -> None:
         self.val = val
@@ -6,67 +15,103 @@ class Node:
 
 
 class Deque:
-    def __init__(self) -> None:
-        """time complexity: O(1)"""
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+    def __init__(self, elements: Optional[list[any]] = []) -> None:
+        """time complexity: O(N)"""
+        self.head = None
+        self.tail = None
         self.length = 0
+
+        for e in elements:
+            self.append(e)
 
     def __len__(self):
         """time complexity: O(1)"""
         return self.length
 
+    def __iter__(self):
+        """time complexity: O(N)"""
+        node = self.head
+        while node:
+            yield node.val
+            node = node.next
+
+    def __getitem__(self, index: int) -> any:
+        """time complexity: O(N)"""
+        _index = index if index >= 0 else self.length + index
+
+        if not 0 <= _index < self.length:
+            raise IndexError("index out of range")
+        node = self.head
+        for _ in range(_index):
+            node = node.next
+        return node.val
+
+    def __setitem__(self, index: int, value: any) -> None:
+        """time complexity: O(N)"""
+        _index = index if index >= 0 else self.length + index
+
+        if not 0 <= _index < self.length:
+            raise IndexError("index out of range")
+
+        node = self.head
+        for _ in range(_index):
+            node = node.next
+        node.val = value
+
+    def __bool__(self) -> bool:
+        return not self.is_empty()
+
     def is_empty(self):
         """time complexity: O(1)"""
         return self.length == 0
 
-    def left(self):
-        """time complexity: O(1)"""
-        if self.is_empty():
-            raise IndexError("deque is empty")
-        return self.head.next.val
-
-    def right(self):
-        """time complexity: O(1)"""
-        if self.is_empty():
-            raise IndexError("deque is empty")
-        return self.tail.prev.val
-
     def append_left(self, val: any) -> None:
         """time complexity: O(1)"""
         node = Node(val)
-        node.next, node.prev = self.head.next, self.head
-        self.head.next.prev, self.head.next = node, node
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
         self.length += 1
 
     def append(self, val: any) -> None:
         """time complexity: O(1)"""
         node = Node(val)
-        node.prev, node.next = self.tail.prev, self.tail
-        node.prev.next, node.next.prev = node, node
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
         self.length += 1
 
     def pop_left(self):
         """time complexity: O(1)"""
         if self.is_empty():
             raise IndexError("not able to pop from empty deque")
-        node = self.head.next
-        self.head.next = node.next
-        node.next.prev = self.head
+        val = self.head.val
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
         self.length -= 1
-        return node.val
+        return val
 
     def pop(self):
         """time complexity: O(1)"""
         if self.is_empty():
             raise IndexError("not able to pop from empty deque")
-        node = self.tail.prev
-        self.tail.prev = node.prev
-        node.prev.next = self.tail
+        val = self.tail.val
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
         self.length -= 1
-        return node.val
+        return val
 
     def remove(self, val: any) -> bool:
         """time complexity: O(N)"""
