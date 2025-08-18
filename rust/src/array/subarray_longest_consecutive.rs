003b967fc19a6760610ac16cdea6310c1e0738cc
Zhenyu Lin <zhenyu.lin@opbi.org>
2025-08-18 23:32:20 +0100

    fix(rust/array): faster subarray longest consecutive sequence

--- rust/src/array/subarray_longest_consecutive.rs
+++ rust/src/array/subarray_longest_consecutive.rs
@@ -1,21 +1,30 @@
 use pyo3::prelude::*;
-use std::collections::HashSet;
+
+/* NOTE: set vs sort
+- for set, each lookup would perform a hash + probe logic, with the probe jumps around RAM
+    - while for sorted array, the memory scan would be linear
+    - especially when the hash algorithm is expensive, the O(N) would be slower than O(NLogN)
+        - std::collections::HashSet uses SipHash, more expensive than AHashSet
+*/
 
 #[pyfunction]
-pub fn longest_consecutive_sequence(nums: Vec<i32>) -> i32 {
-    let set: HashSet<i32> = nums.into_iter().collect();
-    let mut res = 0;
+pub fn longest_consecutive_sequence(mut nums: Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+    nums.sort_unstable();
 
-    for &n in &set {
-        // avoid repeated check for previous intermediate point
-        if !set.contains(&(n - 1)) {
-            let mut m = 1;
-            while set.contains(&(n + m)) {
-                m += 1;
-            }
+    let (mut res, mut m) = (1, 1);
+    for i in 1..nums.len() {
+        if nums[i] == nums[i - 1] {
+            continue;
+        }
+        if nums[i] == nums[i - 1] + 1 {
+            m += 1;
+        } else {
             res = res.max(m);
+            m = 1;
         }
     }
-
-    res
+    res.max(m)
 }
