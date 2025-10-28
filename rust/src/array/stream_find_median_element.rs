82cb449e0eb960c2211718150f3b2aa1c3c93f1d
Zhenyu Lin <zhenyu.lin@opbi.org>
2025-10-28 19:58:03 +0800

    fix(rust/array): stream find median element

--- rust/src/array/stream_find_median_element.rs
+++ rust/src/array/stream_find_median_element.rs
@@ -17,20 +17,27 @@ impl MedianFinder {
         }
     }
 
+    #[inline]
     fn add_num(&mut self, num: i32) {
-        self.right.push(-num);
-        self.left.push(-self.right.pop().unwrap());
+        if self.left.peek().is_none_or(|&x| num <= x) {
+            self.left.push(num);
+        } else {
+            self.right.push(-num);
+        }
 
-        if self.left.len() > self.right.len() {
+        if self.left.len() > self.right.len() + 1 {
             self.right.push(-self.left.pop().unwrap());
+        } else if self.right.len() > self.left.len() {
+            self.left.push(-self.right.pop().unwrap());
         }
     }
 
+    #[inline]
     fn find_median(&self) -> f64 {
         if self.left.len() == self.right.len() {
             (self.left.peek().unwrap() - self.right.peek().unwrap()) as f64 / 2.0
         } else {
-            -self.right.peek().unwrap() as f64
+            *self.left.peek().unwrap() as f64
         }
     }
 }
